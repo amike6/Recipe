11989d7b68e48cbee59ce35ad2c6bcfbcb37454c
Jayson <jaysonamike03@gmail.com>
2023-05-08 19:56:24 -0400

    learning the mechanics of the program

---
+++ Test/index.js
@@ -0,0 +1,64 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DB'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/books', (req, res) => {
+    connection.query('SELECT * FROM books', (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+  app.post('/books', (req, res) => {
+    const { title, author, tags } = req.body;
+    connection.query(`INSERT INTO books (title, author,tags) VALUES ("${title}", "${author}", "${tags}")`, (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+  app.put('/books/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, author } = req.body;
+    connection.query(`UPDATE books SET title="${title}", author="${author}" WHERE id=${id}`, (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+  app.delete('/books/:id', (req, res) => {
+    const { id } = req.params;
+    connection.query(`DELETE FROM books WHERE id=${id}`, (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+
+  // Start the server and listen on port 3000
+app.listen(3001, () => {
+  console.log('Server listening on port 3002');
+});
+  
---
+++ Test/index2.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Use bodyParser to parse form data in request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mock data to store in API
+let books = [
+  {
+    id: 1,
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    tags: ['classic', 'fiction']
+  },
+  {
+    id: 2,
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    tags: ['classic', 'fiction']
+  },
+  {
+    id: 3,
+    title: '1984',
+    author: 'George Orwell',
+    tags: ['dystopian', 'fiction']
+  }
+];
+// Route for creating a new book entry from a form submission
+app.post('/books', (req, res) => {
+  // Generate a new ID for the book
+  const newId = books.length + 1;
+
+  // Create a new book object with the ID and data from the request body
+  const newBook = {
+    id: newId,
+    title: req.body.title,
+    author: req.body.author
+  };
+
+  // Add the new book to the array of books
+  books.push(newBook);
+
+  // Redirect to the home page after adding the new book
+  res.redirect('/display');
+});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+// Route for rendering the home page with the list of books
+app.get('/display', (req, res) => {
+  // Render the list of books as HTML
+  let bookList = '';
+  books.forEach(book => {
+    bookList += `<li>${book.title} by ${book.author}</li>`;
+  });
+
+  const html = `
+    
+        <ul>
+          ${bookList}
+        </ul>
+        
+  `;
+
+  res.send(books);
+});
+
+// Start the server and listen on port 3000
+app.listen(3005, () => {
+  console.log('Server listening on port 3002');
+});
